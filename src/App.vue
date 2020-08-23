b59fd34413c7d44d85532051b94144b71c4f96c5
Magnus Edetorn <magnus.edetorn@tieto.com>
2020-08-23 22:09:05 +0200

    added amplify files

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <amplify-authenticator> -->
+      <!-- <amplify-sign-in header-text="Custom sign-in" slot="sign-in"></amplify-sign-in> -->
+      <img alt="Vue logo" src="./assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--       <div id="amplify-signout">
+        <amplify-sign-out></amplify-sign-out>
+      </div> -->
+    <!-- </amplify-authenticator> -->
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { onAuthUIStateChange } from '@aws-amplify/ui-components'
 
 export default {
   name: 'App',
+  created() {
+    onAuthUIStateChange((authState, authData) => {
+      this.authState = authState;
+      this.user = authData;
+    })
+  },
+  data() {
+    return {
+      user: undefined,
+      authState: undefined
+    }
+  },
+  beforeDestroy() {
+    return onAuthUIStateChange;
+  },
   components: {
     HelloWorld
   }
@@ -24,5 +46,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#amplify-signout {
+  width: 100px;
+  margin: 0 auto;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,52 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+import '@aws-amplify/ui-vue';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
+// var AWS = require("aws-sdk");
+// var macaddress = require('macaddress');
+
+// AWS.config.update({
+// 	region: "eu-north-1"
+// });
+
+// var docClient = new AWS.DynamoDB.DocumentClient();
+// var table = "kalmarmakerspace_tag"
+// var tagId = 12345678
+// var currentMachine = 3
+
+// var params = {
+// 	TableName: table,
+// 	Key:{
+// 		"tag": tagId
+// 	}
+// };
+
+// AWS.config.getCredentials(function(err) {
+//   if (err) console.log(err.stack);
+// 	// credentials not loaded
+//   else {
+// 	console.log("Access key:", AWS.config.credentials.accessKeyId);
+// 	docClient.get(params, function(err, data) {
+// 		if (err) {
+// 			console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+// 		} else {
+// 			console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+// 			console.log("allowedmachines:", data.Item.allowedmachines);
+// 			console.log("includes current machine?:", data.Item.allowedmachines.includes(currentMachine));
+// 		}
+// 	});
+// 	macaddress.one(function (err, mac) {
+// 		console.log("Mac address for this host: %s", mac);
+// 	});
+// 	}
+// });
+
+// Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+	render: h => h(App),
 }).$mount('#app')
